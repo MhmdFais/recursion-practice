c495d5c99e719c038a3c9bab80f709cbcfcaa749
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-06-15 15:00:12 +0530

    Fibonacci done

--- fibonacci.js
+++ fibonacci.js
@@ -8,4 +8,12 @@ function fibs(num) {
   console.log(array);
 }
 
-function fibsRec(num) {}
+function fibsRec(num, arr = [0, 1]) {
+  if (num < 0) return "Incorrect Input";
+  if (num === 0) return [];
+  if (num === 1) return [0];
+  if (arr.length >= num) return arr.slice(0, num);
+
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return fibsRec(num, arr);
+}
